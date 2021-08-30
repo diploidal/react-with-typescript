dae718709e3d230aaa114e785fb2eb1f5fb63954
Diploidal <diploidal@gmail.com>
2021-08-30 18:53:12 +0200

    example of useRef usage

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,14 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 
 export const Input = () => {
   const [name, setName] = useState(''); //Type inference NOT explicit
   // inference - automatic type detection, explicit - tell what type is e.g string
   // const [name, setName] = useState<string>("")
   // const [name, setName] = useState<string | null>(null)
-  return <input value={name} onChange={(e) => setName(e.target.value)}/>
+
+  const ref = useRef<HTMLInputElement>(null!); // ! - stands for READ ONLY
+  if(ref && ref.current) {
+    console.log(ref.current.value)
+  }
+  return <input ref={ref} value={name} onChange={(e) => setName(e.target.value)}/>
 }
